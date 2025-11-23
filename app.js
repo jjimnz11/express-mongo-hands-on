1da578e8b0f5e955b9b22bc54f76fca4027ba068
jjimnz11 <jesusjimenezpozo@gmail.com>
2025-11-23 20:13:24 +0100

    Proyecto terminado: ToDoList de Jesús con dos listas

--- app.js
+++ app.js
@@ -1,138 +1,105 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const _ = require("lodash");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+// VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(bodyParser.urlencoded({extended: true}));
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+// === "BASE DE DATOS" EN MEMORIA ===
+const taskLists = new Map([
+  ["General", []],
+  ["Trabajo", []]
+]);
 
-const itemsSchema = {
-  name: String
-};
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).slice(2, 8);
+}
 
-const Item = mongoose.model("Item", itemsSchema);
-
-
-const item1 = new Item({
-  name: "Welcome to your todolist!"
-});
-
-const item2 = new Item({
-  name: "Hit the + button to add a new item."
-});
-
-const item3 = new Item({
-  name: "<-- Hit this to delete an item."
-});
-
-const defaultItems = [item1, item2, item3];
-
-const listSchema = {
-  name: String,
-  items: [itemsSchema]
-};
-
-const List = mongoose.model("List", listSchema);
-
-
-app.get("/", function(req, res) {
-
-  Item.find({}, function(err, foundItems){
-
-    if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err){
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Successfully savevd default items to DB.");
-        }
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
-    }
-  });
-
-});
-
-app.get("/:customListName", function(req, res){
-  const customListName = _.capitalize(req.params.customListName);
-
-  List.findOne({name: customListName}, function(err, foundList){
-    if (!err){
-      if (!foundList){
-        //Create a new list
-        const list = new List({
-          name: customListName,
-          items: defaultItems
-        });
-        list.save();
-        res.redirect("/" + customListName);
-      } else {
-        //Show an existing list
-
-        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
-      }
-    }
-  });
-
-
-
-});
-
-app.post("/", function(req, res){
-
-  const itemName = req.body.newItem;
-  const listName = req.body.list;
-
-  const item = new Item({
-    name: itemName
-  });
-
-  if (listName === "Today"){
-    item.save();
-    res.redirect("/");
-  } else {
-    List.findOne({name: listName}, function(err, foundList){
-      foundList.items.push(item);
-      foundList.save();
-      res.redirect("/" + listName);
-    });
+function getList(name) {
+  if (!taskLists.has(name)) {
+    taskLists.set(name, []);
   }
+  return taskLists.get(name);
+}
+
+// ================== RUTAS ==================
+
+// Mostrar LAS DOS listas en la misma página
+app.get("/", (req, res) => {
+  res.render("list", {
+    generalItems: getList("General"),
+    workItems: getList("Trabajo")
+  });
 });
 
-app.post("/delete", function(req, res){
-  const checkedItemId = req.body.checkbox;
-  const listName = req.body.listName;
+// Si alguien va a /work, lo mandamos a la pantalla única
+app.get("/work", (req, res) => {
+  res.redirect("/");
+});
 
-  if (listName === "Today") {
-    Item.findByIdAndRemove(checkedItemId, function(err){
-      if (!err) {
-        console.log("Successfully deleted checked item.");
-        res.redirect("/");
-      }
-    });
-  } else {
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-      if (!err){
-        res.redirect("/" + listName);
-      }
-    });
+// About
+app.get("/about", (req, res) => {
+  res.render("about", { pageTitle: "Sobre esta app" });
+});
+
+// ----------------- AÑADIR TAREA -----------------
+app.post("/", (req, res) => {
+  const itemName = req.body.newItem; 
+  const listName = req.body.list;    
+
+  if (!itemName || !listName) {
+    return res.redirect("/");
   }
 
+  const text = itemName.trim();
+  if (!text) {
+    return res.redirect("/");
+  }
 
+  const newTask = {
+    id: generateId(),
+    name: text
+  };
+
+  const list = getList(listName);
+  list.push(newTask);
+
+  // Siempre volvemos a la página principal, que muestra las dos listas
+  res.redirect("/");
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
+// ----------------- BORRAR TAREA -----------------
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox; 
+  const listName = req.body.listName;      
+
+  console.log("BORRANDO =>", listName, checkedItemId);
+
+  if (!checkedItemId || !listName) {
+    return res.redirect("/");
+  }
+
+  const list = getList(listName);
+  const updatedList = list.filter(item => item.id !== checkedItemId);
+  taskLists.set(listName, updatedList);
+
+  // De nuevo, siempre recargamos la pantalla con las dos listas
+  res.redirect("/");
+});
+
+// 404 simple
+app.use((req, res) => {
+  res.status(404).render("about", {
+    pageTitle: "Página no encontrada"
+  });
 });
 
 app.listen(3000, function() {
